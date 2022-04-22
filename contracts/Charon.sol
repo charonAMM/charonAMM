13757f46cd974e563f7a8b22bbcd62a5eb71b493
Nicholas Fett <nicholasfett@Nicholass-MacBook-Pro.local>
2022-04-21 21:32:31 -0400

    interface file

---
+++ contracts/Charon.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract Charon{
+
+    IERC20 public token;
+    uint256 public fee;
+
+    /**
+     * @dev constructor to start
+     * @param _address of token to be deposited
+     */
+    constructor(address _token, uint256 _fee) external{
+        token = _token;
+        fee = _fee;
+    }
+
+    function lpDeposit() external{
+
+    }
+
+    function lpWithdraw() external{
+
+    }
+
+    //read Tellor, add the deposit to the pool and wait for withdraw
+    function oracleDeposit(){
+
+    }
+
+    //withdraw your tokens (like a market order from the other chain)
+    function secretWithdraw(){
+
+    }
+
+}

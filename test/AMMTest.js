c2b753cf2938367e68f1ed14ae53e99eb7acb8ee
Nicholas Fett <nicholasfett@Nicholass-MBP.hsd1.md.comcast.net>
2022-05-05 21:39:40 -0400

    amm test stuff

--- test/AMMTest.js
+++ test/AMMTest.js
@@ -1,34 +1,28 @@
 const { AbiCoder } = require("@ethersproject/abi");
 const { expect } = require("chai");
-const h = require("./helpers/helpers");
 var assert = require('assert');
 const web3 = require('web3');
 const { ethers } = require("hardhat");
-const { stakeAmount } = require("./helpers/helpers");
 const { keccak256 } = require("ethers/lib/utils");
 
-describe("End-to-End Tests - Nine", function() {
-
-  //const tellorMaster = "0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0"
-  const swapFee = 10e16 //1%
-  let tfac,token1,token2,afac,amm
+describe("Test AMM", function() {
+  const swapFee = web3.utils.toWei("0.01") //1%
+  let tfac,token1,token2,afac,amm,accounts;
 
   beforeEach("deploy and setup TellorX", async function() {
+    accounts = await ethers.getSigners();
     //deploy two tokens
-    tfac = await ethers.getContractFactory("contracts/MockERC20.sol:MockERC20");
+    tfac = await ethers.getContractFactory("contracts/mocks/MockERC20.sol:MockERC20");
     token1 = await tfac.deploy("WrappedETH","WETH");
     token2 = await tfac.deploy("Dissapearing Space Monkey","DSM")
     await token1.deployed();
-
     //deploy AMM
     afac = await ethers.getContractFactory("contracts/AMM.sol:AMM");
     amm = await afac.deploy(swapFee);
     await amm.deployed();
-
     //mint initial token supplies (1M)
-    await token1.mint(accounts[0],web3.utils.toWei("1000000"))//1M
-    await token2.mint(accounts[0],web3.utils.toWei("2000000"))//2M
-
+    await token1.mint(accounts[0].address,web3.utils.toWei("1000000"))//1M
+    await token2.mint(accounts[0].address,web3.utils.toWei("2000000"))//2M
     //approve tokens 
     await token1.approve(amm.address,web3.utils.toWei("100000"))//100k
     await token2.approve(amm.address,web3.utils.toWei("200000"))//200k
@@ -37,24 +31,24 @@ describe("End-to-End Tests - Nine", function() {
     await amm.bind(token2.address,web3.utils.toWei("200000"),web3.utils.toWei("1"))
     //finalize pool
     await amm.finalize();
-
   });
   it("e2eTest", async function() {
-
       //user gets tokesn
-      await token1.transfer(accounts[1],web3.utils.toWei("100000"))
-      await token2.transfer(accounts[2],web3.utils.toWei("100000"))
+      await token1.transfer(accounts[1].address,web3.utils.toWei("100000"))
+      await token2.transfer(accounts[2].address,web3.utils.toWei("100000"))
       //user approves tokens
       await token1.connect(accounts[1]).approve(amm.address,web3.utils.toWei("100"))
+      console.log("here")
+      //current balance is init 100000 , each weight is 1e18, init poolSupply is 100e18, totalWeight is 2e18, put 10e18 in, fee 0 
+      let amountOut = await amm.calcPoolOutGivenSingleIn(web3.utils.toWei("100000"),web3.utils.toWei("1"),web3.utils.toWei("100"),web3.utils.toWei("2"),web3.utils.toWei("10"),0)
+      console.log(web3.utils.fromWei(amountOut))
       //user joinPool
-      await amm.connect(accounts[1])joinPool(web3.utils.toWei("10"),web3.utils.toWei("100"))//pool amount out, maxIn
+      await amm.connect(accounts[1]).joinswapExternAmountIn(token1.address,web3.utils.toWei("10"),amountOut)//pool amount out, maxIn
       //user runs swapExactAmountIn (trades with a specific input)
       await amm.connect(accounts[2].swapExactAmountIn(token2.address,web3.utils.toWei("10"),token2.addreess,web3.utils.toWei("5"),2))
-      //user runs swapExactAmountOut (variable amount in?)
-
-        //test other functions?  What are they all and which one do we want? 
+      //test other functions?  What are they all and which one do we want? 
       //Test AMM pool token interactions
+      await amm.connect(accounts[1]).transfer(accounts[3].address,web3.utils.toWei("5"));
+      assert(await amm.balanceOf(accounts[3]) == web3.utils.toWei["5"], "transfer should work")
   });
-
-
 });
